b4de088b9697a78f0546e9e2aa546d11dc914395
AdriBalla <adriball.dev@gmail.com>
2018-05-11 10:50:55 +0200

    Correction bugs et ajout scripts

--- clear.sh
+++ clear.sh
@@ -3,3 +3,8 @@
 docker-compose kill && docker-compose rm -f
 
 docker volume prune --force
+
+rm -rf ./laravel/vendor
+
+rm ./laravel/composer.lock
+
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose kill && docker-compose rm -f && docker-compose up -d "$@"
+docker-compose kill && docker-compose rm -f && docker-compose up -d --build
 
 docker exec -ti babylog_fpm /bin/bash -c "composer install"
