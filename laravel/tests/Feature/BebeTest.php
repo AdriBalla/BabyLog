8006e8924b1ec4e171080e4eec106521b840d62b
AdriBalla <adriball.dev@gmail.com>
2018-05-12 20:54:20 +0200

    Tests API bébé

--- laravel/tests/Feature/BebeTest.php
+++ laravel/tests/Feature/BebeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Bebe;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -9,23 +10,95 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class BebeTest extends TestCase
 {
     /**
-     * A basic test example.
+     * test insert bebe API.
      *
      * @return void
      */
     public function testInsertBebe(){
 
         $data = array();
-        $data["nom"] = "toto";
-        $data["prenom"] = "tutu";
+        $data["nom"] = "nom";
+        $data["prenom"] = "prenom";
         $data["date_naissance"] = "2018-07-05";
         $data["sexe"] = "M";
-        $data["photo"] = null;
+        $data["photo"] = "url/vers/la/photo";
 
         $result = $this->post('/api/bebes',$data);
         $bebe = $result->json();
 
+        $this->assertEquals($bebe["nom"],"nom");
+        $this->assertEquals($bebe["prenom"],"prenom");
+        $this->assertEquals($bebe["date_naissance"],"2018-07-05 00:00:00");
+        $this->assertEquals($bebe["sexe"],"M");
+        $this->assertEquals($bebe["photo"],"url/vers/la/photo");
+
         $this->assertDatabaseHas('bebe', ['id_bebe' => $bebe["id_bebe"]]);
+
+        $bebe = Bebe::find($bebe["id_bebe"]);
+        $bebe->delete();
     }
 
+    /**
+     * test insert bebe API.
+     *
+     * @return void
+     */
+    public function testUpdateBebe(){
+
+        $data = array();
+        $data["nom"] = "nom";
+        $data["prenom"] = "prenom";
+        $data["date_naissance"] = "2018-07-05";
+        $data["sexe"] = "M";
+        $data["photo"] = "url/vers/la/photo";
+
+        $result = $this->post('/api/bebes',$data);
+        $id_bebe = $result->json()["id_bebe"];
+
+        $data["nom"] = "nom2";
+        $data["prenom"] = "prenom2";
+        $data["date_naissance"] = "2018-06-05";
+        $data["sexe"] = "F";
+        $data["photo"] = "url/vers/la/nouvelle/photo";
+
+        $result = $this->post("api/bebes/".$id_bebe,$data);
+        $bebe = $result->json();
+
+        $this->assertEquals($bebe["nom"],"nom2");
+        $this->assertEquals($bebe["prenom"],"prenom2");
+        $this->assertEquals($bebe["date_naissance"],"2018-06-05 00:00:00");
+        $this->assertEquals($bebe["sexe"],"F");
+        $this->assertEquals($bebe["photo"],"url/vers/la/nouvelle/photo");
+
+        $this->assertDatabaseHas('bebe', ['id_bebe' => $bebe["id_bebe"]]);
+
+        $bebe = Bebe::find($bebe["id_bebe"]);
+        $bebe->delete();
+    }
+
+
+    /**
+     * test insert bebe API.
+     *
+     * @return void
+     */
+    public function testDestroyBebe(){
+
+        $data = array();
+        $data["nom"] = "nom";
+        $data["prenom"] = "prenom";
+        $data["date_naissance"] = "2018-07-05";
+        $data["sexe"] = "M";
+        $data["photo"] = "url/vers/la/photo";
+
+        $result = $this->post('/api/bebes',$data);
+        $id_bebe = $result->json()["id_bebe"];
+
+        $this->assertDatabaseHas('bebe', ['id_bebe' => $id_bebe]);
+
+        $result = $this->delete("api/bebes/".$id_bebe);
+
+        $this->assertDatabaseMissing('bebe', ['id_bebe' => $id_bebe]);
+
+    }
 }
